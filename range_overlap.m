8f98fdcec834f59fbb5b0483f12e191c5200daf9
Rogier Landman <landmanr@gmail.com>
2020-12-15 13:02:24 -0500

    Add files via upload

--- range_overlap.m
+++ range_overlap.m
@@ -1,5 +1,28 @@
 function resmat = range_overlap(arr1,arr2)
 
+%arr1 and arr2 are M by N matrices where each row is a range 
+%column 1 is the start of the range
+%column2 is the end of the range
+
+%output resmat is a matrix with dimensions [size(arr1,1)] x [size(arr2,1)]
+%where each element gives the fraction overlap for a combination of ranges
+%that is, the fraction of arr1 that is being overlapped by arr2
+
+%example
+%arr1=[1 5; 10 15; 20 25];
+%arr2=[6 9; 10 17; 24 29];
+
+%first row of arr1: no overlap with any range of arr2
+%second row of arr1: 100% overlapped by second row of arr2
+%third row of arr1: 20% overlapped by third row of arr2
+
+%output:
+% resmat =
+% 
+%          0         0         0
+%          0    1.0000         0
+%          0         0    0.2000
+
 %rogierlandman 2020
 
 resmat = zeros(size(arr1,1), size(arr2,1));
@@ -11,7 +34,6 @@ for i=1:size(arr1,1)
     for j=1:size(arr2,1)
         startpoint2 = arr2(j,1);
         endpoint2 = arr2(j,2);
-        dur2 = endpoint2 - startpoint2;
         overlap = 1;
         if (endpoint1 < startpoint2)
             %no overlap
@@ -26,11 +48,11 @@ for i=1:size(arr1,1)
             startofoverlap = max([startpoint1,startpoint2]);
             endofoverlap = min([endpoint1,endpoint2]);
             %how much of arr1 is captured?
-            dur3 = endofoverlap - startofoverlap;
-            if(dur3>dur1)
+            dur2 = endofoverlap - startofoverlap;
+            if(dur2>dur1)
                 overlap=1;
             else
-                overlap=dur3/dur1;
+                overlap=dur2/dur1;
             end
         end
         resmat(i,j) = overlap;
